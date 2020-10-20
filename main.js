85c573bf7d55b95176b623487cbe05023fc1446b
Kicki Halmos <kickihalmos@gmail.com>
2020-10-20 14:58:24 +0200

    test_slideshow

--- main.js
+++ main.js
@@ -1,30 +1,39 @@
-document.addEventListener("DOMContentLoaded", function (e) {
-    var slideIndex = 1;
-showSlides(slideIndex);
+document.querySelector(".prevBtn").addEventListener("click", () => {
+    changeSlides(-1);
+ });
+ document.querySelector(".nextBtn").addEventListener("click", () => {
+    changeSlides(1);
+ });
+ let slideIndex = 1;
+ showSlides(slideIndex);
+ function changeSlides(n) {
+    showSlides((slideIndex += n));
+    
+ }
+ function currentSlide(n) {
+    showSlides((slideIndex = n));
+ }
+ function showSlides(n) {
+    
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-});
+    let slides = document.getElementsByClassName("Slide");
+    for (slide of slides){
+        slide.style.display = "none";
+    }
+    let dots = document.getElementsByClassName("Navdot");
+    for (dot of dots){
+        dot.classList.remove("selected");
+    }
+    if (n > slides.length) {
+       slideIndex = 1;
+    }
+    if (n < 1) {
+       slideIndex = slides.length;
+    }
+    //Array.from(slides).forEach(item => (item.style.display = "none"));
+    //Array.from(dots).forEach(
+       //item => (item.className = item.className.replace(" selected", ""))
+    //);
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " selected";
+ }
---
+++ slide_show.js
@@ -0,0 +1,5 @@
+class Slide_show {
+    constructor() {
+        
+    }
+}
